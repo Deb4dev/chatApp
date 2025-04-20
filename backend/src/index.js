4571a50e3fea52f8f1d659840073eb215a572861
Deb4Dev <chakrabortydebargha575@gmail.com>
2025-04-21 01:50:17 +0530

    updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser'
 import { connectDB } from './lib/db.js'
 import {router as authRouter} from './routes/auth.route.js'
 import  messageRouter from './routes/message.route.js'
+import cors from 'cors'
 
 dotenv.config()
 
@@ -13,6 +14,14 @@ const   PORT = process.env.PORT
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors(
+  {
+    origin: "http://localhost:5173",
+    credentials:true,
+  }
+))
+
+
 app.use("/api/auth",authRouter)
 app.use("/api/message",messageRouter)
 app.listen(PORT, () => {
